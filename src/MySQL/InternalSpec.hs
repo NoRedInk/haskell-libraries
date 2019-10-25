3b15850e994c206e38217703bf9553f410a8cd18
Jasper Woudenberg <mail@jasperwoudenberg.com>
2019-10-25 08:55:14 +0100

    Add test for `MySQL.Internal.anyToIn`

--- src/MySQL/InternalSpec.hs
+++ src/MySQL/InternalSpec.hs
@@ -1,11 +1,34 @@
 module MySQL.InternalSpec (tests) where
 
+import qualified Expect
 import qualified MySQL.Internal
 import Nri.Prelude
-import Test (Test, describe)
+import Test (Test, describe, test)
+import qualified Text
 
 tests :: Test
 tests =
   describe
     "MySQL.Internal"
-    []
+    [ anyToInTests
+    ]
+
+anyToInTests :: Test
+anyToInTests =
+  describe
+    "anyToIn"
+    [ test "Replaces ANY query with IN query" <| \_ ->
+        Text.join
+          "\n"
+          [ "SELECT hat FROM royalty",
+            "WHERE hat = ANY ('{\"crown\", \"fedora\", \"cap\"}');"
+          ]
+          |> MySQL.Internal.anyToIn
+          |> Expect.equal
+            ( Text.join
+                "\n"
+                [ "SELECT hat FROM royalty",
+                  "WHERE hat IN (\"crown\", \"fedora\", \"cap\");"
+                ]
+            )
+    ]
