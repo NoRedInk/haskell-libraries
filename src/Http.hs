5241d694894c38dbf18657de91832f6120f7186a
Tereza Sokol <terezasokol@gmail.com>
2019-11-27 09:16:20 -0600

    Return Task from Http api

--- src/Http.hs
+++ src/Http.hs
@@ -8,19 +8,20 @@ import qualified Network.HTTP.Client.TLS as TLS
 import qualified Network.HTTP.Types.Header as Header
 import qualified Network.HTTP.Types.Status as Status
 import Nri.Prelude
+import Platform
 
 -- |
-newtype Handler
-  = Handler HTTP.Manager
+data Handler
+  = Handler Platform.DoAnythingHandler HTTP.Manager
 
 -- |
 handler :: Conduit.Acquire Handler
 handler =
-  map Handler TLS.newTlsManager
+  map2 Handler (liftIO Platform.doAnythingHandler) TLS.newTlsManager
 
 -- | This is for external libraries only!
 withThirdParty :: Handler -> (HTTP.Manager -> a) -> a
-withThirdParty (Handler manager) library =
+withThirdParty (Handler _ manager) library =
   library manager
 
 -- QUICKS
@@ -33,7 +34,7 @@ instance Aeson.FromJSON Anything where
   parseJSON _ = pure Anything
 
 -- |
-get :: Aeson.FromJSON expect => Handler -> Text -> IO (Result Error expect)
+get :: Aeson.FromJSON expect => Handler -> Text -> Task Error expect
 get handler' url =
   request handler' Settings
     { _method = "GET",
@@ -44,7 +45,7 @@ get handler' url =
     }
 
 -- |
-post :: Aeson.FromJSON expect => Handler -> Text -> IO (Result Error expect)
+post :: Aeson.FromJSON expect => Handler -> Text -> Task Error expect
 post handler' url =
   request handler' Settings
     { _method = "POST",
@@ -67,42 +68,41 @@ data Settings body
       }
 
 -- |
-request :: (Aeson.ToJSON body, Aeson.FromJSON expect) => Handler -> Settings body -> IO (Result Error expect)
-request (Handler manager) settings = do
-  basicRequest <-
-    HTTP.parseRequest <| toS (_url settings)
-  let finalRequest =
-        basicRequest
-          { HTTP.method = toS (_method settings),
-            HTTP.requestHeaders = _headers settings,
-            HTTP.requestBody = HTTP.RequestBodyLBS <| Aeson.encode (_body settings),
-            HTTP.responseTimeout = HTTP.responseTimeoutMicro <| fromIntegral <| Maybe.withDefault 30 (_timeout settings)
-          }
-  response <-
-    try (HTTP.httpLbs finalRequest manager)
-  pure
-    <| case response of
-      Right okResponse ->
-        case Aeson.eitherDecode (HTTP.responseBody okResponse) of
-          Right decodedBody ->
-            Ok decodedBody
-          Left message ->
-            Err (BadBody (toS message))
-      Left (HTTP.HttpExceptionRequest _ content) ->
-        case content of
-          HTTP.StatusCodeException res _ ->
-            let statusCode = fromIntegral << Status.statusCode << HTTP.responseStatus
-             in Err (BadStatus (statusCode res))
-          HTTP.ResponseTimeout ->
-            Err Timeout
-          HTTP.ConnectionTimeout ->
-            Err NetworkError
-          HTTP.ConnectionFailure _ ->
-            Err NetworkError
-          _ ->
-            Err BadResponse
-      Left (HTTP.InvalidUrlException _ message) ->
-        Err (BadUrl (toS message))
+request :: (Aeson.ToJSON body, Aeson.FromJSON expect) => Handler -> Settings body -> Task Error expect
+request (Handler doAnythingHandler manager) settings =
+  doAnything doAnythingHandler <| do
+    basicRequest <-
+      HTTP.parseRequest <| toS (_url settings)
+    let finalRequest =
+          basicRequest
+            { HTTP.method = toS (_method settings),
+              HTTP.requestHeaders = _headers settings,
+              HTTP.requestBody = HTTP.RequestBodyLBS <| Aeson.encode (_body settings),
+              HTTP.responseTimeout = HTTP.responseTimeoutMicro <| fromIntegral <| Maybe.withDefault 30 (_timeout settings)
+            }
+    response <- try (HTTP.httpLbs finalRequest manager)
+    pure <| case response of
+        Right okResponse ->
+          case Aeson.eitherDecode (HTTP.responseBody okResponse) of
+            Right decodedBody ->
+              Ok decodedBody
+            Left message ->
+              Err (BadBody (toS message))
+        Left (HTTP.HttpExceptionRequest _ content) ->
+          case content of
+            HTTP.StatusCodeException res _ ->
+              let statusCode = fromIntegral << Status.statusCode << HTTP.responseStatus
+               in Err (BadStatus (statusCode res))
+            HTTP.ResponseTimeout ->
+              Err Timeout
+            HTTP.ConnectionTimeout ->
+              Err NetworkError
+            HTTP.ConnectionFailure _ ->
+              Err NetworkError
+            _ ->
+              Err BadResponse
+        Left (HTTP.InvalidUrlException _ message) ->
+          Err (BadUrl (toS message))
 
 -- |
 data Error
