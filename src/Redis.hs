c0bb4595bbbeda12b77358758347f2835d1dab2e
mavnn <michael@mavnn.co.uk>
2020-04-09 18:37:53 +0100

    Add `get` and `set` commands

--- src/Redis.hs
+++ src/Redis.hs
@@ -1,6 +1,7 @@
 module Redis
   ( Handler,
     handler,
+    Namespace (..),
     -- Settings
     Settings.Settings,
     Settings.decoder,
@@ -15,12 +16,20 @@ import qualified Data.Text.Encoding
 import qualified Database.Redis
 import qualified Platform
 import qualified Redis.Settings as Settings
+import qualified Task
+import Prelude (Either (Left, Right), pure)
 
 data Handler
   = Handler
       { anything :: Platform.DoAnythingHandler,
         connection :: Database.Redis.Connection
       }
+
+data Error = Error
+  deriving (Show, Eq)
+
+newtype Namespace = Namespace Text
+  deriving (Show, Eq)
 
 toT :: Data.ByteString.ByteString -> Text
 toT = Data.Text.Encoding.decodeUtf8
@@ -34,4 +43,31 @@ handler settings =
   where
     acquire = do
       anything <- Platform.doAnythingHandler
-      connection <- Database.Redis.connect
+      connection <- Database.Redis.checkedConnect (Settings.connectionInfo settings)
+      pure <| Handler anything connection
+    release Handler {connection} = Database.Redis.disconnect connection
+
+namespaceKey :: Namespace -> Text -> Data.ByteString.ByteString
+namespaceKey (Namespace ns) key =
+  ns ++ ":" ++ key |> toB
+
+platformRedis :: Handler -> Database.Redis.Redis (Either Database.Redis.Reply a) -> Task Error a
+platformRedis Handler {connection, anything} action =
+  Platform.doAnything
+    anything
+    ( Database.Redis.runRedis connection action
+        |> map
+          ( \rep -> case rep of
+              Left _ -> Err Error
+              Right r -> Ok r
+          )
+    )
+
+set :: Handler -> Namespace -> Text -> Text -> Task Error Database.Redis.Status
+set handler ns key value =
+  platformRedis handler (Database.Redis.set (namespaceKey ns key) (toB value))
+
+get :: Handler -> Namespace -> Text -> Task Error (Maybe Text)
+get handler ns key =
+  platformRedis handler (Database.Redis.get (namespaceKey ns key))
+    |> map (map toT)
--- src/Redis/Settings.hs
+++ src/Redis/Settings.hs
@@ -1,21 +1,32 @@
-module Redis.Settings (Settings, decoder) where
+module Redis.Settings (Settings (..), decoder) where
 
 import Cherry.Prelude
 import qualified Data.Text
-import Database.Redis
+import Database.Redis hiding (Ok)
 import qualified Environment
+import Prelude (Either (Left, Right))
 
 newtype Settings
   = Settings
-      { connectionString :: Text
+      { connectionInfo :: ConnectInfo
       }
 
 decoder :: Environment.Decoder Settings
-decoder =
+decoder = map Settings decoderConnectInfo
+
+decoderConnectInfo :: Environment.Decoder ConnectInfo
+decoderConnectInfo =
   Environment.variable
     Environment.Variable
       { Environment.name = "REDIS_CONNECTION_STRING",
         Environment.description = "Full redis connection string",
         Environment.defaultValue = "redis://localhost:6379"
       }
-    (Environment.text |> map Settings)
+    ( Environment.custom
+        Environment.text
+        ( \str ->
+            case str |> Data.Text.unpack |> parseConnectInfo of
+              Right info -> Ok info
+              Left parseError -> Err ("Invalid Redis connection string: " ++ Data.Text.pack parseError)
+        )
+    )
