fdeb0445c86faafae5882c461d78f16e418e1285
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-07-17 09:08:21 +0100

    Disable transaction fuzz test

--- src/DatabaseSpec.hs
+++ src/DatabaseSpec.hs
@@ -13,16 +13,22 @@ import qualified Internal.Query.ParserSpec
 import qualified Internal.TimeSpec
 import qualified MySQL.InternalSpec
 import qualified MySQLSpec
-import qualified MySQLTransactionSpec
 import qualified Postgres.SettingsSpec
 import Test (Test, describe)
 import qualified Test.Runner.Tasty
 import qualified Prelude
 
+-- `Test.Runner.Tasty.main` exits after finishing, so run other tests first.
+--
+-- We're not running this spec: MySQLTransactionSpec.main
+-- The spec runs a fuzz test against the database. Reasonbly fast locally but
+-- not in CI. It's not using our standard test framework so it doesn't include
+-- Junit output when it fails either.
+--
+-- The test was useful for debugging a problem and so we keep it around, but
+-- it's not essential as a regression test.
 main :: Prelude.IO ()
-main = do
-  -- `Test.Runner.Tasty.main` exits after finishing, so run other tests first.
-  MySQLTransactionSpec.main
+main =
   Test.Runner.Tasty.main DatabaseSpec.tests
 
 tests :: Test
