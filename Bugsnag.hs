73371f0cb6e335cc7bdccd4d386361d19282f49e
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-08-08 14:26:11 +0200

    Find root cause and log stack trace in Bugsnag

--- Bugsnag.hs
+++ Bugsnag.hs
@@ -9,10 +9,12 @@ where
 
 import Cherry.Prelude
 import qualified Control.Exception.Safe as Exception
+import qualified Data.List
 import qualified Data.Proxy as Proxy
 import qualified Data.Text
 import qualified Data.Typeable as Typeable
 import qualified Environment
+import qualified GHC.Stack as Stack
 import qualified Health
 import qualified Http
 import qualified Log
@@ -42,25 +44,58 @@ send http settings event = do
 toEvent :: Maybe Exception.SomeException -> Platform.Span -> Bugsnag.Event
 toEvent _ span =
   Bugsnag.defaultEvent
-    { Bugsnag.event_exceptions = [toException span]
+    { Bugsnag.event_exceptions = [rootCause [] span]
     }
 
-toException :: Platform.Span -> Bugsnag.Exception
-toException span =
+-- | Find the most recently started span that failed. This span is closest to
+-- the failure and we'll use the data in it and its parents to build the
+-- exception we send to Bugsnag. We'll send information about spans that ran
+-- before the root cause span started as breadcrumbs.
+rootCause :: [Bugsnag.StackFrame] -> Platform.Span -> Bugsnag.Exception
+rootCause frames span =
+  let newFrames =
+        case Platform.frame span of
+          Nothing -> frames
+          Just (name, src) -> toStackFrame name src : frames
+   in case Data.List.find failed (Platform.children span) of
+        Nothing -> toException newFrames span
+        Just child -> rootCause newFrames child
+
+failed :: Platform.Span -> Bool
+failed span =
+  case Platform.succeeded span of
+    Platform.Succeeded -> False
+    Platform.Failed -> True
+    Platform.FailedWith _ -> True
+
+toException :: [Bugsnag.StackFrame] -> Platform.Span -> Bugsnag.Exception
+toException frames span =
   case Platform.succeeded span of
     Platform.Succeeded -> Bugsnag.defaultException
     Platform.Failed ->
       Bugsnag.defaultException
-        { Bugsnag.exception_errorClass = "Failed: " ++ Platform.name span
+        { Bugsnag.exception_errorClass = "Failed: " ++ Platform.name span,
+          Bugsnag.exception_stacktrace = frames
         }
     Platform.FailedWith (Exception.SomeException exception) ->
       Bugsnag.defaultException
         { Bugsnag.exception_errorClass = typeName exception,
+          Bugsnag.exception_stacktrace = frames,
           Bugsnag.exception_message =
             Exception.displayException exception
               |> Data.Text.pack
               |> Just
         }
+
+toStackFrame :: Text -> Stack.SrcLoc -> Bugsnag.StackFrame
+toStackFrame functionName frame =
+  Bugsnag.defaultStackFrame
+    { Bugsnag.stackFrame_file = Data.Text.pack (Stack.srcLocFile frame),
+      Bugsnag.stackFrame_lineNumber = Stack.srcLocStartLine frame,
+      Bugsnag.stackFrame_columnNumber = Just (Stack.srcLocStartCol frame),
+      Bugsnag.stackFrame_method = functionName,
+      Bugsnag.stackFrame_inProject = Just True
+    }
 
 typeName :: forall a. Typeable.Typeable a => a -> Text
 typeName _ =
