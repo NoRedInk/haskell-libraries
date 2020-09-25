64dad0ba6e8c05b4a36fbccac2b3b26d6d979cde
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-09-25 09:11:48 +0100

    Change to Observability.NewRelic
    To ensure we're pulling in the latest version of our `tracing-newrelic`
    dependency, because I don't believe dependency bumps are picked up by CI
    compilation.

--- NewRelic.hs
+++ NewRelic.hs
@@ -1,4 +1,4 @@
--- | Report request tracing data to NewRelic.
+-- | Report request tracing data to NewRelic APM.
 --
 -- NewRelic helps us get statistics about requests, such as average response
 -- times, error rates, and break downs of how much time is spent doing what
