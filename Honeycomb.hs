86fef4013c51c1cbaf3351dc3295a1e0b8adc421
Juliano Solanho <juliano.solanho@gmail.com>
2020-09-29 14:39:29 -0300

    We're getting 202 responses back!
    * Changed the payload to conform to HC's
    * Actually send the request to the correct URL
    * Use `request` so we can send headers

--- Honeycomb.hs
+++ Honeycomb.hs
@@ -23,66 +23,130 @@ where
 
 import Cherry.Prelude
 import qualified Conduit
-import qualified Debug
-import qualified List
-import qualified Data.Aeson as Aeson
 -- import qualified Data.Text
-import qualified Environment
+
 -- import qualified GHC.Stack as Stack
-import qualified Data.UUID as UUID
-import Data.UUID.V4 (nextRandom)
-import qualified Http
+
 -- import qualified Language.Haskell.TH as TH
 -- import qualified Maybe
+
+import Control.Monad.IO.Class (liftIO)
+import qualified Data.Aeson as Aeson
+import Data.Aeson ((.=))
+import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Encoding
+import qualified Data.UUID as UUID
+import Data.UUID.V4 (nextRandom)
+import qualified Debug
+import qualified Environment
+import qualified Http
+import qualified List
+import qualified Log
 import Observability.Timer (Timer, toISO8601)
 import qualified Platform
-import qualified Prelude
 import qualified Task
+import qualified Prelude
+
+batchApiEndpoint :: Text -> Text
+batchApiEndpoint datasetName = "https://api.honeycomb.io/1/batch/" ++ datasetName
 
 report :: Handler -> Text -> Platform.TracingSpan -> Prelude.IO ()
 report handler' requestId span = do
-  spans <- toSpans handler' requestId Nothing span
+  spans <- toBatchEvents handler' requestId Nothing span
   let body = Http.jsonBody spans
   silentHandler' <- Platform.silentHandler
-  result <- Http.post (handler_http handler') "idk" body Http.expectWhatever
-              |> Task.attempt (silentHandler')
+  let datasetName = (handler_serviceName handler') ++ "-" ++ (handler_environment handler')
+  let url = batchApiEndpoint datasetName
+  let requestSettings = Http.Settings
+        { Http._method = "POST",
+          Http._headers = [("X-Honeycomb-Team", Encoding.encodeUtf8 <| Log.unSecret <| handler_honeycombApiKey handler')],
+          Http._url = url,
+          Http._body = body,
+          Http._timeout = Nothing,
+          Http._expect = Http.expectText
+        }
+  result <-
+    Http.request (handler_http handler') requestSettings
+      |> Task.attempt (silentHandler')
   case result of
-    Ok () -> Prelude.pure ()
-    Err _wedontcareyet -> Prelude.pure ()
+    Ok body' -> do
+      Prelude.putStrLn "OK"
+      Prelude.putStrLn <| Text.unpack body'
+      Prelude.pure ()
+    Err err -> do
+      _ <- Prelude.fail <| Prelude.show err
+      Prelude.putStrLn "ERROR"
+      Prelude.pure ()
 
-toSpans :: Handler -> Text -> Maybe SpanId -> Platform.TracingSpan -> Prelude.IO [Span]
-toSpans handler' requestId parentSpanId span = do
+toBatchEvents :: Handler -> Text -> Maybe SpanId -> Platform.TracingSpan -> Prelude.IO [BatchEvent]
+toBatchEvents handler' requestId parentSpanId span = do
   thisSpansId <- map SpanId nextRandom
-  children <- Prelude.traverse (toSpans handler' requestId (Just thisSpansId)) (Platform.children span)
+  children <- Prelude.traverse (toBatchEvents handler' requestId (Just thisSpansId)) (Platform.children span)
   let duration = (Platform.finished span) - (Platform.started span) |> Platform.inMicroseconds
   let timestamp' = toISO8601 (handler_timer handler') (Platform.started span)
-  Prelude.pure <| Span {
-    timestamp = timestamp',
-    name = Platform.name span,
-    spanId = thisSpansId,
-    parentId = parentSpanId,
-    traceId = requestId,
-    serviceName = handler_serviceName handler',
-    environment = handler_environment handler',
-    durationMs = (Prelude.fromIntegral duration) / 1000
-  } : (List.concat children)
+  let hcSpan = Span
+        { timestamp = timestamp',
+          name = Platform.name span,
+          spanId = thisSpansId,
+          parentId = parentSpanId,
+          traceId = requestId,
+          serviceName = handler_serviceName handler',
+          environment = handler_environment handler',
+          durationMs = (Prelude.fromIntegral duration) / 1000
+        }
+  Prelude.pure <| BatchEvent
+    { batchevent_time = timestamp',
+      batchevent_data = hcSpan
+    }
+    : (List.concat children)
 
-data Span = Span {
-  timestamp :: Text,
-  name :: Text,
-  spanId :: SpanId,
-  parentId :: Maybe SpanId,
-  traceId :: Text,
-  serviceName :: Text,
-  environment :: Text,
-  durationMs :: Float
-}
+data BatchEvent
+  = BatchEvent
+      { batchevent_time :: Text,
+        batchevent_data :: Span
+      }
   deriving (Generic)
 
-instance Aeson.ToJSON Span
+options :: Aeson.Options
+options =
+  Aeson.defaultOptions
+    { -- Drop the batchevent_ prefix
+      Aeson.fieldLabelModifier = List.drop 1 << Prelude.dropWhile ('_' /=)
+    }
+
+instance Aeson.ToJSON BatchEvent where
+  toJSON = Aeson.genericToJSON options
+
+data Span
+  = Span
+      { timestamp :: Text,
+        name :: Text,
+        spanId :: SpanId,
+        parentId :: Maybe SpanId,
+        traceId :: Text,
+        serviceName :: Text,
+        environment :: Text,
+        durationMs :: Float
+      }
+  deriving (Generic)
+
+instance Aeson.ToJSON Span where
+  toJSON span =
+    -- Use honeycomb's field names, srcs:
+    -- https://docs.honeycomb.io/getting-data-in/tracing/send-trace-data/#manual-tracing
+    -- https://docs.honeycomb.io/working-with-your-data/managing-your-data/definitions/
+    Aeson.object
+      [ "name" .= name span,
+        "trace.span_id" .= spanId span,
+        "trace.parent_id" .= parentId span,
+        "trace.trace_id" .= traceId span,
+        "service_name" .= serviceName span,
+        "duration_ms" .= durationMs span
+        -- TODO: put other fields here
+      ]
 
 newtype SpanId = SpanId UUID.UUID
-  deriving Aeson.ToJSON
+  deriving (Aeson.ToJSON)
 
 data Handler
   = Handler
@@ -91,35 +155,37 @@ data Handler
         handler_timer :: Timer,
         handler_http :: Http.Handler,
         handler_serviceName :: Text,
-        handler_environment :: Text
+        handler_environment :: Text,
+        handler_honeycombApiKey :: Log.Secret Text
       }
 
 handler :: Timer -> Settings -> Conduit.Acquire Handler
 handler timer settings = do
   http <- Http.handler
-  Prelude.pure Handler {
-    handler_timer = timer,
-    handler_http = http,
-    handler_serviceName = appName settings,
-    handler_environment = appEnvironment settings
-  }
+  Prelude.pure Handler
+    { handler_timer = timer,
+      handler_http = http,
+      handler_serviceName = appName settings,
+      handler_environment = appEnvironment settings,
+      handler_honeycombApiKey = honeycombApiKey settings
+    }
 
 data Settings
   = Settings
       { appName :: Text,
-        appEnvironment :: Text
+        appEnvironment :: Text,
+        honeycombApiKey :: Log.Secret Text
       }
 
 decoder :: Environment.Decoder Settings
 decoder =
   Debug.todo "hihi"
-  -- Prelude.pure Settings
-  --   |> andMap logFileDecoder
-  --   |> andMap namespaceDecoder
-  --   |> andMap environmentDecoder
-  --   |> andMap fractionOfSuccessRequestsLoggedDecoder
-  --   -- We don't define decoders for the mock* fields used by tests. Tests can
-  --   -- construct a Settings object directly without decoding it from env vars.
-  --   |> andMap (Prelude.pure Nothing)
-  --   |> andMap (Prelude.pure Nothing)
-
+-- Prelude.pure Settings
+--   |> andMap logFileDecoder
+--   |> andMap namespaceDecoder
+--   |> andMap environmentDecoder
+--   |> andMap fractionOfSuccessRequestsLoggedDecoder
+--   -- We don't define decoders for the mock* fields used by tests. Tests can
+--   -- construct a Settings object directly without decoding it from env vars.
+--   |> andMap (Prelude.pure Nothing)
+--   |> andMap (Prelude.pure Nothing)
