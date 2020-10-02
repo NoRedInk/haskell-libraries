333e1ce3330ce015124dba41291eff6612b1236d
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-10-02 18:04:09 +0100

    Move all CI code into `test.sh`
    This makes it easier to reproduce failures locally, by running the same
    script.

--- test.sh
+++ test.sh
@@ -2,11 +2,17 @@
 
 set -euo pipefail
 
+# Check all Haskell files are formatted using Ormolu
 LC_ALL=C.UTF-8 ormolu -m inplace nri-*/**/*.hs
-
 if git status --porcelain | grep . ; then
   echo "Not all files were formatted with ormolu."
-  echo "Run ./format.sh and commit the result."
+  echo "To fix this error, run ./test.sh and commit the result."
   git diff --no-color
   exit 1
 fi
+
+# Run Haskell tests
+hpack nri-prelude
+hpack env-parser
+cabal update
+cabal test all
