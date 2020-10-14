0cb0b65b954fe87dd8e180dd2828fddc6ae46432
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-10-14 11:43:16 +0100

    Make Redis.Mock data model explicit
    This is a set-up step to changing this model.

--- src/Redis/Mock.hs
+++ src/Redis/Mock.hs
@@ -7,7 +7,7 @@ where
 
 import Data.ByteString (ByteString)
 import qualified Data.HashMap.Strict as HM
-import Data.IORef (atomicModifyIORef', newIORef)
+import Data.IORef (IORef, atomicModifyIORef', newIORef)
 import qualified Database.Redis
 import qualified List
 import Nri.Prelude
@@ -18,58 +18,83 @@ import Prelude (IO, error, pure)
 
 handler :: Text -> IO Internal.Handler
 handler namespace = do
-  hm <- newIORef HM.empty
+  model <- init
   anything <- Platform.doAnythingHandler
   Internal.InternalHandler
     { Internal.doQuery = \query ->
-        atomicModifyIORef' hm (doQuery query)
+        atomicModifyIORef' model (doQuery query)
           |> Platform.doAnything anything,
       Internal.doTransaction = \query ->
-        atomicModifyIORef' hm (doQuery query)
+        atomicModifyIORef' model (doQuery query)
           |> Platform.doAnything anything,
       Internal.watch = \_ -> pure () -- TODO: implement this
     }
     |> Internal.namespacedHandler namespace
     |> Prelude.pure
 
+-- | This is our mock implementation of the Redis state. Our mock implementation
+-- will store a single value of this type, and redis commands will modify it.
+newtype Model = Model {hash :: HM.HashMap ByteString ByteString}
+
+withHash :: (HM.HashMap ByteString ByteString -> HM.HashMap ByteString ByteString) -> Model -> Model
+withHash f (Model hm) = Model (f hm)
+
+init :: IO (IORef Model)
+init = newIORef (Model HM.empty)
+
 doQuery ::
   Internal.Query a ->
-  HM.HashMap ByteString ByteString ->
-  (HM.HashMap ByteString ByteString, Result Internal.Error a)
-doQuery query hm =
+  Model ->
+  (Model, Result Internal.Error a)
+doQuery query model =
   case query of
-    Internal.Ping -> (hm, Ok Database.Redis.Pong)
-    Internal.Get key -> (hm, Ok <| HM.lookup key hm)
-    Internal.Set key value -> (HM.insert key value hm, Ok ())
-    Internal.Getset key value -> (HM.insert key value hm, Ok <| HM.lookup key hm)
-    Internal.Mget keys -> (hm, Ok <| List.map (\key -> HM.lookup key hm) keys)
+    Internal.Ping ->
+      ( model,
+        Ok Database.Redis.Pong
+      )
+    Internal.Get key ->
+      ( model,
+        Ok <| HM.lookup key (hash model)
+      )
+    Internal.Set key value ->
+      ( withHash (HM.insert key value) model,
+        Ok ()
+      )
+    Internal.Getset key value ->
+      ( withHash (HM.insert key value) model,
+        Ok <| HM.lookup key (hash model)
+      )
+    Internal.Mget keys ->
+      ( model,
+        Ok <| List.map (\key -> HM.lookup key (hash model)) keys
+      )
     Internal.Mset assocs ->
       ( List.foldl
-          (\(key, val) hm' -> HM.insert key val hm')
-          hm
+          (\(key, val) model' -> withHash (HM.insert key val) model')
+          model
           assocs,
         Ok ()
       )
     Internal.Del keys ->
       List.foldl
-        ( \key (hm', count) ->
-            if HM.member key hm'
-              then (HM.delete key hm', count + 1)
-              else (hm', count)
+        ( \key (model', count) ->
+            if HM.member key (hash model')
+              then (withHash (HM.delete key) model', count + 1)
+              else (model', count)
         )
-        (hm, 0 :: Int)
+        (model, 0 :: Int)
         keys
         |> Tuple.mapSecond Ok
     Internal.Hgetall _key -> error "No mock implementation implemented yet for hgetall"
     Internal.Hset _key _field _val -> error "No mock implementation implemented yet for hset"
     Internal.Hmset _key _vals -> error "No mock implementation implemented yet for hset"
-    Internal.Pure x -> (hm, Ok x)
+    Internal.Pure x -> (model, Ok x)
     Internal.Apply fQuery xQuery ->
-      let (hm1, f) = doQuery fQuery hm
+      let (hm1, f) = doQuery fQuery model
           (hm2, x) = doQuery xQuery hm1
        in (hm2, map2 (\f' x' -> f' x') f x)
     Internal.WithResult f q ->
-      doQuery q hm
+      doQuery q model
         |> map
           ( \result ->
               case result of
