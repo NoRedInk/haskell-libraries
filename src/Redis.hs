b68e681eb9d56422fafb31dbb7059660c1ffe227
Stoeffel <schtoeffel@gmail.com>
2020-12-03 16:23:24 +0100

    Add docs to api functions

--- src/Redis.hs
+++ src/Redis.hs
@@ -24,9 +24,26 @@ module Redis
 
     -- * Creating api access functions
     makeApi,
-    Api (..),
+    Api,
+    del,
+    expire,
+    get,
+    getset,
+    mget,
+    mset,
+    ping,
+    rpush,
+    set,
+    setnx,
     makeHashApi,
-    HashApi (..),
+    HashApi,
+    hdel,
+    hget,
+    hgetall,
+    hmget,
+    hmset,
+    hset,
+    hsetnx,
     Decoder,
     Encoder,
     jsonDecoder,
@@ -68,15 +85,64 @@ type Decoder a = ByteString -> Result Internal.Error a
 
 data Api key a
   = Api
-      { del :: List.List key -> Internal.Query Int,
+      { -- | Removes the specified keys. A key is ignored if it does not exist.
+        --
+        -- https://redis.io/commands/del
+        del :: List.List key -> Internal.Query Int,
+        -- | Set a timeout on key. After the timeout has expired, the key will
+        -- automatically be deleted. A key with an associated timeout is often said to
+        -- be volatile in Redis terminology.
+        --
+        -- https://redis.io/commands/expire
         expire :: key -> Int -> Internal.Query (),
+        -- | Get the value of key. If the key does not exist the special value Nothing
+        -- is returned. An error is returned if the value stored at key is not a
+        -- string, because GET only handles string values.
+        --
+        -- https://redis.io/commands/get
         get :: key -> Internal.Query (Maybe a),
+        -- | Atomically sets key to value and returns the old value stored at key.
+        -- Returns an error when key exists but does not hold a string value.
+        --
+        -- https://redis.io/commands/getset
         getset :: key -> a -> Internal.Query (Maybe a),
+        -- | Returns the values of all specified keys. For every key that does not hold
+        -- a string value or does not exist, no value is returned. Because of this, the
+        -- operation never fails.
+        --
+        -- https://redis.io/commands/mget
         mget :: Ord key => List.List key -> Internal.Query (Dict.Dict key a),
+        -- | Sets the given keys to their respective values. MSET replaces existing
+        -- values with new values, just as regular SET. See MSETNX if you don't want to
+        -- overwrite existing values.
+        --
+        -- MSET is atomic, so all given keys are set at once. It is not possible for
+        -- clients to see that some of the keys were updated while others are
+        -- unchanged.
+        --
+        -- https://redis.io/commands/mset
         mset :: Dict.Dict key a -> Internal.Query (),
+        -- | Returns PONG if no argument is provided, otherwise return a copy of the
+        -- argument as a bulk. This command is often used to test if a connection is
+        -- still alive, or to measure latency.
+        --
+        -- https://redis.io/commands/ping
         ping :: Internal.Query (),
+        -- | Insert all the specified values at the tail of the list stored at key. If key does not exist, it is created as empty list before performing the push operation. When key holds a value that is not a list, an error is returned.
+        --
+        -- https://redis.io/commands/rpush
         rpush :: key -> List.List a -> Internal.Query Int,
+        -- | Set key to hold the string value. If key already holds a value, it is
+        -- overwritten, regardless of its type. Any previous time to live associated
+        -- with the key is discarded on successful SET operation.
+        --
+        -- https://redis.io/commands/set
         set :: key -> a -> Internal.Query (),
+        -- | Set key to hold the string value. If key already holds a value, it is
+        -- overwritten, regardless of its type. Any previous time to live associated
+        -- with the key is discarded on successful SET operation.
+        --
+        -- https://redis.io/commands/set
         setnx :: key -> a -> Internal.Query Bool
       }
 
@@ -105,12 +171,43 @@ makeApi encode decode toKey =
 
 data HashApi key field a
   = HashApi
-      { hdel :: key -> List.List field -> Internal.Query Int,
+      { -- | Removes the specified fields from the hash stored at key. Specified fields
+        -- that do not exist within this hash are ignored. If key does not exist, it is
+        -- treated as an empty hash and this command returns 0.
+        --
+        -- https://redis.io/commands/hdel
+        hdel :: key -> List.List field -> Internal.Query Int,
+        -- | Get the value of the field of a hash at key. If the key does not exist,
+        -- or the field in the hash does not exis the special value Nothing is returned
+        -- An error is returned if the value stored at key is not a
+        -- hash, because HGET only handles string values.
+        --
+        -- https://redis.io/commands/hget
         hget :: key -> field -> Internal.Query (Maybe a),
+        -- | Returns all fields and values of the hash stored at key. In the returned value, every field name is followed by its value, so the length of the reply is twice the size of the hash.
+        -- Nothing in the returned value means failed utf8 decoding, not that it doesn't exist
+        --
+        -- https://redis.io/commands/hgetall
         hgetall :: key -> Internal.Query (Dict.Dict field a),
+        -- | Returns the values associated with the specified fields in the hash stored at key.--
+        --
+        -- equivalent to modern hget
+        -- https://redis.io/commands/hmget
         hmget :: key -> List.List field -> Internal.Query (Dict.Dict field a),
+        -- | Sets fields in the hash stored at key to values. If key does not exist, a new key holding a hash is created. If any fields exists, they are overwritten.
+        --
+        -- equivalent to modern hset
+        -- https://redis.io/commands/hmset
         hmset :: key -> Dict.Dict field a -> Internal.Query (),
+        -- | Sets field in the hash stored at key to value. If key does not exist, a new key holding a hash is created. If field already exists in the hash, it is overwritten.
+        --
+        -- https://redis.io/commands/hset
         hset :: key -> field -> a -> Internal.Query (),
+        -- | Sets field in the hash stored at key to value, only if field does not yet
+        -- exist. If key does not exist, a new key holding a hash is created. If field
+        -- already exists, this operation has no effect.
+        --
+        -- https://redis.io/commands/hsetnx
         hsetnx :: key -> field -> a -> Internal.Query Bool
       }
 
