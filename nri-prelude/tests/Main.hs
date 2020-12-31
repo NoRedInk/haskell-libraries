d820e5c2a75117cad80eee0635b0e29a004bc376
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-12-31 17:16:05 +0100

    Set default file encoding for tests
    Seeing some weird encoding related errors in Travis CI test runs, and
    hope this might solve them.

--- nri-prelude/tests/Main.hs
+++ nri-prelude/tests/Main.hs
@@ -7,16 +7,20 @@ import qualified ArraySpec
 import qualified BitwiseSpec
 import qualified DebugSpec
 import qualified DictSpec
+import qualified GHC.IO.Encoding
 import qualified LogSpec
 import qualified PlatformSpec
 import qualified SetSpec
+import qualified System.IO
 import Test (Test, describe, run)
 import qualified TestSpec
 import qualified TextSpec
 import qualified Prelude
 
 main :: Prelude.IO ()
-main = run tests
+main = do
+  GHC.IO.Encoding.setLocaleEncoding System.IO.utf8
+  run tests
 
 tests :: Test
 tests =
