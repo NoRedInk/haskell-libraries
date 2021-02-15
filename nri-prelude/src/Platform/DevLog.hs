e29edb80885e3f3b01428642767a87081f283c39
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-02-15 11:59:49 +0100

    Prevent interleaving when writing to dev log

--- nri-prelude/src/Platform/DevLog.hs
+++ nri-prelude/src/Platform/DevLog.hs
@@ -1,10 +1,18 @@
-module Platform.DevLog (writeSpanToDevLog) where
+{-# OPTIONS_GHC -fno-cse #-}
 
+module Platform.DevLog
+  ( writeSpanToDevLog,
+  )
+where
+
+import qualified Control.Concurrent.MVar as MVar
 import qualified Data.Aeson as Aeson
 import qualified Data.ByteString.Lazy
 import qualified Data.Time as Time
+import NriPrelude
 import qualified Platform.Internal
 import qualified System.IO
+import qualified System.IO.Unsafe
 import qualified Prelude
 
 -- | Write a tracing span to the development log, where it can be found by
@@ -13,10 +21,19 @@ writeSpanToDevLog :: Platform.Internal.TracingSpan -> Prelude.IO ()
 writeSpanToDevLog span = do
   now <- Time.getCurrentTime
   let logFile = "/tmp/nri-prelude-logs"
-  System.IO.withFile
-    logFile
-    System.IO.AppendMode
-    ( \handle -> do
-        Data.ByteString.Lazy.hPut handle (Aeson.encode (now, span))
-        Data.ByteString.Lazy.hPut handle "\n"
-    )
+  MVar.withMVar writeLock <| \_ ->
+    System.IO.withFile
+      logFile
+      System.IO.AppendMode
+      ( \handle -> do
+          Data.ByteString.Lazy.hPut handle (Aeson.encode (now, span))
+          Data.ByteString.Lazy.hPut handle "\n"
+      )
+
+-- A lock used to ensure writing spans to the dev log are atomic, processes will
+-- take turns to fully write their spans to the log to prevent interleaving.
+{-# NOINLINE writeLock #-}
+writeLock :: MVar.MVar ()
+writeLock =
+  MVar.newMVar ()
+    |> System.IO.Unsafe.unsafePerformIO
