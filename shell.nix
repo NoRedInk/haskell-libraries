1367589064b27fb7357a7cc7e20ff5c415c087a0
Michael Glass <me@mike.is>
2021-02-17 11:28:54 +0100

    pull in haskell packages from nix

--- shell.nix
+++ shell.nix
@@ -2,10 +2,38 @@ let
   sources = import ./nix/sources.nix { };
 
   pkgs = import sources.nixpkgs { };
-
 in pkgs.mkShell {
   buildInputs = [
-    pkgs.haskell.compiler.ghc865
+    (pkgs.haskellPackages.ghcWithPackages (haskellPackges:
+    with haskellPackges; [
+      (pkgs.haskell.lib.doJailbreak (pkgs.haskell.lib.markUnbroken pretty-diff))
+      aeson
+      aeson-pretty
+      ansi-terminal
+      async
+      auto-update
+      base
+      brick
+      bytestring
+      containers
+      directory
+      exceptions
+      filepath
+      ghc
+      hedgehog
+      junit-xml
+      modern-uri
+      network-uri
+      pretty-show
+      process
+      safe-exceptions
+      terminal-size
+      text
+      time
+      unordered-containers
+      vector
+      vty
+    ]))
     pkgs.cabal-install
     pkgs.haskellPackages.hpack
     pkgs.zlib
