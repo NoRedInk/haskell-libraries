be913159c7ebaa867d54fd55bea03fd9c33e99c4
Michael Glass <me@mike.is>
2021-02-17 11:08:48 +0100

    export all the defaults and more

--- nri-prelude/src/NriPrelude/Plugin.hs
+++ nri-prelude/src/NriPrelude/Plugin.hs
@@ -3,7 +3,9 @@
 -- | A compiler plugin that imports the equivalents of modules that Elm will
 -- also import.
 --
+--
 -- Useful documentation
+-- - Elm's default imports: https://package.elm-lang.org/packages/elm/core/latest/
 -- - GHC user guide on compiler plugins: https://ghc.gitlab.haskell.org/ghc/doc/users_guide/extending_ghc.html#compiler-plugins
 -- - Module providing API for creating plugins: https://www.stackage.org/haddock/lts-17.4/ghc-lib-8.10.4.20210206/GhcPlugins.html
 module NriPrelude.Plugin
@@ -42,9 +44,21 @@ addImplicitImports _ _ parsed =
   where
     addImports hsModule =
       hsModule {hsmodImports = hsmodImports hsModule ++ extraImports}
+    -- taken from https://package.elm-lang.org/packages/elm/core/latest/
     extraImports =
-      [ unqualified "NriPrelude",
-        qualified "Debug"
+      [ unqualified "NriPrelude", -- Elm exports types from withi these modules. We re-export them from NriPrelude. Same effect.
+        qualified "Basics",
+        qualified "Char",
+        qualified "Debug",
+        qualified "List",
+        qualified "Maybe",
+        qualified "Platform",
+        qualified "Result",
+        qualified "Text", -- equivalent to Elm's String
+        qualified "Tuple",
+        -- Additionally Task and Log because we use them everywhere
+        qualified "Log",
+        qualified "Task"
       ]
     unqualified name =
       GhcPlugins.noLoc (simpleImportDecl (GhcPlugins.mkModuleName name))
