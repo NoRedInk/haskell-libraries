d9aa212a8c87cdddd1b52c5709f8a5899292c2fd
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-02-19 12:31:04 +0100

    Picking ghc 8-10 from unstable branch
    Because this version is built by the nix server farm Hydra, so it will
    already be in caches.

--- shell-ghc-8-10.nix
+++ shell-ghc-8-10.nix
@@ -1,8 +1,8 @@
 let
   sources = import ./nix/sources.nix { };
 
-  pkgs = import sources.nixpkgs { };
+  pkgs = import sources.nixpkgs-unstable { };
 in import nix/mk-shell.nix {
   pkgs = pkgs;
-  haskellPackages = pkgs.haskell.packages.ghc8103;
+  haskellPackages = pkgs.haskell.packages.ghc8104;
 }
