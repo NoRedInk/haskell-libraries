21a2c8a5fefa7e0426232b2e7e12f075061a8184
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-02-21 09:42:33 +0100

    Add log-explorer installation instructions

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 let
-  sources = import ../nix/sources.nix { };
+  sources = import ./nix/sources.nix { };
   pkgs = import sources.nixpkgs { };
   haskellPackages = pkgs.haskellPackages.extend (self: super: {
-    nri-prelude = super.callCabal2nix "nri-prelude" ../nri-prelude { };
-    log-explorer = super.callCabal2nix "log-explorer" ./. { };
+    nri-prelude = super.callCabal2nix "nri-prelude" ./nri-prelude { };
+    log-explorer = super.callCabal2nix "log-explorer" ./nri-log-explorer { };
     pretty-diff = super.callCabal2nix "pretty-diff" sources.pretty-diff { };
   });
 
