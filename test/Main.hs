f76a8eea93b65ed7948225ff2d353192cdafa35a
Jasper Woudenberg <jasper@noredink.com>
2021-02-24 12:45:19 +0100

    Standardize library test setup
    In a previous commit we made `aide watch` run tests in libraries. To do
    this it needs to watch the `spec` suite rather than the library itself.
    For this to provide a nice experience that `spec` suite should know both
    about the test and regular source code. This sets up our libraries to do
    this all correctly.

--- test/Main.hs
+++ test/Main.hs
@@ -16,14 +16,6 @@ import qualified TimeSpec
 import qualified Prelude
 
 -- `Test.run` exits after finishing, so run other tests first.
---
--- We're not running this spec: MySQLTransactionSpec.main
--- The spec runs a fuzz test against the database. Reasonbly fast locally but
--- not in CI. It's not using our standard test framework so it doesn't include
--- Junit output when it fails either.
---
--- The test was useful for debugging a problem and so we keep it around, but
--- it's not essential as a regression test.
 main :: Prelude.IO ()
 main = do
   postgresSettings <- Environment.decode Postgres.decoder
