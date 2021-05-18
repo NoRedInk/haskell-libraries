27091e5794e89806afc23425f90223640ce56d59
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-05-18 16:44:15 +0200

    Don't expose non empty lists

--- nri-prelude/src/Examples.hs
+++ nri-prelude/src/Examples.hs
@@ -6,31 +6,34 @@
 -- our types change, potentially in backwards-incompatible ways.
 module Examples
   ( HasExamples (..),
-    Example (..),
+    Examples,
     example,
     concat,
-    toList,
   )
 where
 
 import qualified Data.Aeson
 import qualified Data.Aeson.Encode.Pretty
 import qualified Data.ByteString.Lazy
+import qualified Data.List.NonEmpty as NonEmpty
 import Data.Proxy (Proxy (Proxy))
 import qualified Data.Text.Encoding
 import qualified Dict
 import NriPrelude
-import qualified Text
+import qualified Prelude
 
--- | An example consists of a description and an encoded value.
+-- | Example values of a type.
+newtype Examples = Examples (NonEmpty.NonEmpty Example)
+
 data Example = Example
   { description :: Text,
     encodedValue :: Text
   }
   deriving (Eq, Ord)
 
--- | Helper to produce and `Example`
-example :: Data.Aeson.ToJSON a => Text -> a -> Example
+-- | Create an example for a type. Examples consists of a description and an
+-- encoded value.
+example :: Data.Aeson.ToJSON a => Text -> a -> Examples
 example description x =
   Example
     { description,
@@ -39,19 +42,18 @@ example description x =
           |> Data.ByteString.Lazy.toStrict
           |> Data.Text.Encoding.decodeUtf8
     }
+    |> Prelude.pure
+    |> Examples
 
 -- | A helper type class that provides us example values of particular types.
 -- The `IsApi` typeclass below will demand we define an instance of this type
 -- class for each type used in a request or response body.
 class HasExamples t where
-  examples :: Proxy t -> (Example, List Example)
+  examples :: Proxy t -> Examples
 
 -- | Concat two "nonempty" lists of examples together.
-concat :: (a, List a) -> (a, List a) -> (a, List a)
-concat (x, xs) (y, ys) = (x, xs ++ (y : ys))
-
-toList :: (a, List a) -> List a
-toList (x, xs) = x : xs
+concat :: Examples -> Examples -> Examples
+concat (Examples xs) (Examples ys) = Examples (xs ++ ys)
 
 instance (HasExamples a, HasExamples b) => HasExamples (a, b) where
   examples _ = concat (examples (Proxy :: Proxy a)) (examples (Proxy :: Proxy b))
@@ -78,7 +80,7 @@ instance (HasExamples a) => HasExamples (List a) where
   examples _ = examples (Proxy :: Proxy a)
 
 instance HasExamples Int where
-  examples _ = (example "int" (1 :: Int), [])
+  examples _ = example "int" (1 :: Int)
 
 instance HasExamples () where
-  examples _ = (example "unit" (), [])
+  examples _ = example "unit" ()
