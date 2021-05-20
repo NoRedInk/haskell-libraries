f7ba51cf2c008d25d9f5c412786dfbe98818c6cb
Stoeffel <schtoeffel@gmail.com>
2021-05-20 13:49:59 +0200

    Use typeable to get test name and golden file name

--- nri-redis/src/Redis/Test.hs
+++ nri-redis/src/Redis/Test.hs
@@ -2,11 +2,20 @@
 module Redis.Test (encoding) where
 
 import Data.Proxy (Proxy (Proxy))
+import qualified Data.Typeable as Typeable
 import qualified Examples
 import Test (Test)
 import qualified Test.Encoding
+import qualified Text
 
-encoding :: forall m key a. Examples.HasExamples a => Text -> Text -> m key a -> Test
-encoding name fileName _ =
-  Examples.examples (Proxy :: Proxy a)
-    |> Test.Encoding.examplesToTest name fileName
+encoding :: forall m key a. (Typeable.Typeable a, Examples.HasExamples a) => m key a -> Test
+encoding _ =
+  let proxy = Proxy :: Proxy a
+      tyCon =
+        Typeable.typeRep proxy
+          |> Typeable.typeRepTyCon
+      typeName =
+        Typeable.tyConModule tyCon ++ "." ++ Typeable.tyConName tyCon
+          |> Text.fromList
+   in Examples.examples proxy
+        |> Test.Encoding.examplesToTest typeName ("redis-encoding-" ++ typeName)
