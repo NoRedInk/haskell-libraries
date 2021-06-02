c18696e57eb5af7241403d5d6c9b8592c198d76a
Michael Glass <me@mike.is>
2021-06-02 12:36:22 +0200

    name shell based on ghc, symlink shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,1 @@
-let
-  sources = import ./nix/sources.nix { };
-
-  pkgs = import sources.nixpkgs { };
-in import nix/mk-shell.nix {
-  pkgs = pkgs;
-  haskellPackages = pkgs.haskell.packages.ghc884;
-}
+shell-ghc-8-8.nix
