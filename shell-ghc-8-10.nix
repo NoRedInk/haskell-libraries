92af9885c1ecc8df8a902f22d3d1ca88fef9dcb6
Michael Glass <me@mike.is>
2021-06-02 12:42:53 +0200

    default is nixpkgs-21.05, version shell-ghc-8-10

--- shell-ghc-8-10.nix
+++ shell-ghc-8-10.nix
@@ -1,7 +1,7 @@
 let
   sources = import ./nix/sources.nix { };
 
-  pkgs = import sources.nixpkgs-unstable { };
+  pkgs = import sources.nixpkgs-21-05 { };
 in import nix/mk-shell.nix {
   pkgs = pkgs;
   haskellPackages = pkgs.haskell.packages.ghc8104;
