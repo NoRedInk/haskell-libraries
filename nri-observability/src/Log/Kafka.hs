c3f96bcc3bc1c342e8f4c122e765a6740d837727
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-06-17 10:01:49 +0200

    Document remaining Kafka fields

--- nri-observability/src/Log/Kafka.hs
+++ nri-observability/src/Log/Kafka.hs
@@ -7,17 +7,31 @@ import qualified Data.Aeson as Aeson
 import qualified Data.Time.Clock as Clock
 import qualified Platform
 
--- | A type describing kafka message.
+-- | A type describing a kafka message being processed by a consumer.
 data Consumer = Consumer
-  { topic :: Text,
+  { -- | The topic name of the message.
+    topic :: Text,
+    -- | The partition id of the message.
     partitionId :: Int,
+    -- | The key of the message (if it has one). If a key is provided by a
+    -- message producer it is used to determine the partition id, in such a way
+    -- that messages with the same key are guaranteed to end up in the same
+    -- partition.
     key :: Maybe Text,
+    -- | The contents of the message.
+    --
+    -- This library is currently assuming all message payloads use JSON. That's
+    -- not a limitation of Kafka itself though.
     contents :: Encodable,
-    -- | The meaning of this is depending on `log.message.timestamp.type`.
-    -- Fortunately, `Consumer.Timestamp` is a union and contains the
-    -- information about this.
+    -- | The time at which this message was created by a producer.
+    -- Whether this property is available for a message depends on the
+    -- `log.message.timestamp.type` configuration option.
     -- More context: https://github.com/edenhill/librdkafka/blob/8bacbc0b4c357193288c81277bfcc815633126ea/INTRODUCTION.md#latency-measurement
     createTime :: Maybe Clock.UTCTime,
+    -- | The time at which this message was added to a log by a broker.
+    -- Whether this property is available for a message depends on the
+    -- `log.message.timestamp.type` configuration option.
+    -- More context: https://github.com/edenhill/librdkafka/blob/8bacbc0b4c357193288c81277bfcc815633126ea/INTRODUCTION.md#latency-measurement
     logAppendTime :: Maybe Clock.UTCTime,
     -- | Zero-based counter indicating the how-manyth time it is we're attemping
     -- to process this message.
