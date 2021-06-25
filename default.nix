dc09e4bb4d04e85d0fbb54c948b59e5d1498e0c1
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-06-25 10:36:10 +0200

    Remove pretty-diff override

--- default.nix
+++ default.nix
@@ -4,7 +4,6 @@ let
   haskellPackages = pkgs.haskellPackages.extend (self: super: {
     nri-prelude = super.callCabal2nix "nri-prelude" ./nri-prelude { };
     log-explorer = super.callCabal2nix "log-explorer" ./nri-log-explorer { };
-    pretty-diff = super.callCabal2nix "pretty-diff" sources.pretty-diff { };
     safe-coloured-text = super.callCabal2nix "safe-coloured-text"
       "${sources.safe-coloured-text}/safe-coloured-text" { };
     safe-coloured-text-terminfo =
