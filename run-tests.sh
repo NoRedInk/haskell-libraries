959647d95450569bf7fb571f6d5dfcef30769558
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-06-25 11:30:24 +0200

    Fix run-tests.sh script
    This was failing if the postgres data directory didn't exist yet.

--- run-tests.sh
+++ run-tests.sh
@@ -3,12 +3,12 @@
 set -euxo pipefail
 
 ## start postgres
+mkdir -p "_build/postgres/data"
 PGDATA="$(realpath _build/postgres/data)"
 export PGDATA
 export PGUSER=$USER
 pg_ctl stop || true
 rm -rf "$PGDATA"
-mkdir -p "$PGDATA"
 initdb
 pg_ctl start -o '-k .'
 
