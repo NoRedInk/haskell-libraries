caaae4c39e2438b2722a11920ff49a7f98f02744
Michael Glass <me@mike.is>
2021-06-30 18:02:38 +0200

    don't run tests until kafka starts

--- run-tests.sh
+++ run-tests.sh
@@ -19,6 +19,22 @@ redis-server --daemonize yes --dir ./_build/redis/data
 ## start kafka
 server_properties_path=$(dirname "$(which kafka-server-start.sh)")/../config/server.properties
 kafka-server-start.sh -daemon "$server_properties_path" --override num.partitions=10
+# wait for zookeeper
+echo "waiting for zookeeper to start"
+until nc -vz localhost 2181
+do
+  sleep 1
+done 
+echo "zookeeper available"
+
+# wait for kafka
+echo "waiting for kafka to start"
+until  nc -vz localhost 9092
+do
+  sleep 1
+done
+echo "kafka available"
+
 
 cabal build all
 cabal test all
