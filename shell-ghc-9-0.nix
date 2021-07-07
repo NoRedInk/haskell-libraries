fad60566be6608207f3d7609166a7575092c39d3
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-07-07 16:26:26 +0200

    Grab ghc-9.0 from nixpkgs-unstable
    The 21.09 release branch has a ghc-9.0 compiler too, but some of the
    Haskell dependencies we need don't seem to compile yet. The unstable
    ghc-9.0 is doing better in that regard.

--- shell-ghc-9-0.nix
+++ shell-ghc-9-0.nix
@@ -1,7 +1,7 @@
 let
   sources = import ./nix/sources.nix { };
 
-  pkgs = import sources.nixpkgs { };
+  pkgs = import sources.nixpkgs-unstable { };
 in import nix/mk-shell.nix {
   pkgs = pkgs;
   haskellPackages = pkgs.haskell.packages.ghc901;
