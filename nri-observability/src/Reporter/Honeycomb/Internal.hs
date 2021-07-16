0081c88025d13e1c138a31f33742e5e74e23fa6e
Ian Davies <ian@noredink.com>
2021-07-16 10:32:32 +0100

    Add modifyX methods to settings for apdex and sample rate

--- nri-observability/src/Reporter/Honeycomb/Internal.hs
+++ nri-observability/src/Reporter/Honeycomb/Internal.hs
@@ -571,7 +571,19 @@ data Settings = Settings
     --
     -- [@environment variable@] HONEYCOMB_APDEX_TIME_IN_MILLISECONDS
     -- [@default value@] 100
-    apdexTimeMs :: Int
+    apdexTimeMs :: Int,
+    -- | Allows overriding the default sample rates for given spans.
+    -- This allows us to change the sample rate for certain endpoints within an
+    -- application, for example if a path is critical but low volume we may choose
+    -- to increase the rate.
+    -- [@default value@] the input float
+    modifyFractionOfSuccessRequestsLogged :: Float -> Platform.TracingSpan -> Float,
+    -- | Allows overriding the default apdex rates for given spans.
+    -- This allows us to change the apdex for certain endpoints within an
+    -- application, for example if a path is significantly lower volume than
+    -- another the apdex may require tuning.
+    -- [@default value@] the input int
+    modifyApdexTimeMs :: Int -> Platform.TracingSpan -> Int
   }
 
 -- | Read 'Settings' from environment variables. Default variables will be used
@@ -584,6 +596,8 @@ decoder =
     |> andMap honeycombAppNameDecoder
     |> andMap fractionOfSuccessRequestsLoggedDecoder
     |> andMap apdexTimeMsDecoder
+    |> andMap (Prelude.pure always)
+    |> andMap (Prelude.pure always)
 
 honeycombApiKeyDecoder :: Environment.Decoder (Log.Secret ByteString.ByteString)
 honeycombApiKeyDecoder =
