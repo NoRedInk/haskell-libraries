8547c6a59f37dc37f50c9a42b4959daf8c3e69a9
Brian Hicks <brian@brianthicks.com>
2021-12-22 08:58:20 -0600

    use GHC 8.10.7

--- shell-ghc-8-10.nix
+++ shell-ghc-8-10.nix
@@ -4,7 +4,7 @@ let
   pkgs = import sources.nixpkgs { };
 in import nix/mk-shell.nix {
   pkgs = pkgs;
-  haskellPackages = pkgs.haskell.packages.ghc8104.extend (self: super: {
+  haskellPackages = pkgs.haskell.packages.ghc8107.extend (self: super: {
     safe-coloured-text = super.callCabal2nix "safe-coloured-text"
       "${sources.safe-coloured-text}/safe-coloured-text" { };
     safe-coloured-text-terminfo =
