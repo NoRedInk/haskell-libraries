f8dff2475505a466cd3372dc0810133f3a3d739a
Stoeffel <schtoeffel@gmail.com>
2022-04-14 14:24:45 +0200

    Add topLevel metric types

--- nri-kafka/src/Kafka/Stats.hs
+++ nri-kafka/src/Kafka/Stats.hs
@@ -10,6 +10,7 @@ import Data.Aeson.Extra (Path, pathToText)
 import qualified Data.Aeson.Extra as Aeson.Extra
 import Data.ByteString (ByteString)
 import Dict (Dict)
+import qualified Dict
 
 type Stats = Dict Aeson.Extra.Path Aeson.Value
 
@@ -17,3 +18,31 @@ type StatsCallback = (Result Text Stats -> Task Text ())
 
 decode :: ByteString -> Result Text Stats
 decode raw = Aeson.Extra.decodeIntoFlatDict raw
+
+data Value = StringValue | IntValue | IntGauge
+
+topLevel :: Dict Text Value
+topLevel =
+  Dict.fromList
+    [ ("name", StringValue),
+      ("client_id", StringValue),
+      ("type", StringValue),
+      ("ts", IntValue),
+      ("time", IntValue),
+      ("age", IntValue),
+      ("replyq", IntGauge),
+      ("msg_cnt", IntGauge),
+      ("msg_size", IntGauge),
+      ("msg_max", IntValue),
+      ("msg_size_max", IntValue),
+      ("tx", IntValue),
+      ("tx_bytes", IntValue),
+      ("rx", IntValue),
+      ("rx_bytes", IntValue),
+      ("txmsgs", IntValue),
+      ("txmsg_bytes", IntValue),
+      ("rxmsgs", IntValue),
+      ("rxmsg_bytes", IntValue),
+      ("simple_cnt", IntGauge),
+      ("metadata_cache_cnt", IntGauge)
+    ]
