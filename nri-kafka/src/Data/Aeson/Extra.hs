84a8d28d8def556d45a0360fc84208117eac2eea
Stoeffel <schtoeffel@gmail.com>
2022-04-14 13:21:57 +0200

    Less casing

--- nri-kafka/src/Data/Aeson/Extra.hs
+++ nri-kafka/src/Data/Aeson/Extra.hs
@@ -49,30 +49,27 @@ decodeIntoFlatDict :: ByteString -> Result Text (Dict Path Aeson.Value)
 decodeIntoFlatDict content =
   case Aeson.eitherDecodeStrict content of
     Left err -> Err (Text.fromList err)
-    Right value ->
-      case value of
-        Aeson.Object obj ->
-          obj
-            |> HM.foldlWithKey' (\acc' k -> objectToDict [] acc' (Key k)) Dict.empty
-            |> Ok
-        Aeson.Array arr -> Ok (arrayToDict [] Dict.empty arr)
-        _ -> Ok (Dict.singleton [] value)
+    Right value -> Ok (valueToDict [] Dict.empty Nothing value)
 
-objectToDict :: Path -> Dict Path Aeson.Value -> Segment -> Aeson.Value -> Dict Path Aeson.Value
-objectToDict path acc segment val =
-  case val of
-    Aeson.Object obj ->
-      HM.foldlWithKey' (\acc' k -> objectToDict (segment : path) acc' (Key k)) acc obj
-    Aeson.Array arr -> arrayToDict (segment : path) acc arr
-    _ -> Dict.insert (List.reverse (segment : path)) val acc
+valueToDict :: Path -> Dict Path Aeson.Value -> Maybe Segment -> Aeson.Value -> Dict Path Aeson.Value
+valueToDict path acc maybeSegment val =
+  let newPath =
+        case maybeSegment of
+          Nothing -> path
+          Just segment -> segment : path
+   in case val of
+        Aeson.Object obj ->
+          HM.foldlWithKey' (\acc' k -> valueToDict newPath acc' (Just (Key k))) acc obj
+        Aeson.Array arr -> arrayToDict newPath acc arr
+        _ -> Dict.insert (List.reverse newPath) val acc
 
 arrayToDict :: Path -> Dict Path Aeson.Value -> Aeson.Array -> Dict Path Aeson.Value
 arrayToDict path =
   Vector.ifoldl
     ( \acc index item ->
-        objectToDict
+        valueToDict
           path
           acc
-          (Index <| Prelude.fromIntegral index)
+          (Just (Index (Prelude.fromIntegral index)))
           item
     )
