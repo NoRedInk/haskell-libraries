9f88c74990104fb08a783e38575389a14a25e0df
Charlie Hanley <charles.scott.hanley+git@gmail.com>
2022-08-05 14:53:23 -0400

    For tests of Task.parallel and Task.concurrently, verify that execution time implies they're actually execution stuff concurrently

--- nri-prelude/tests/TaskSpec.hs
+++ nri-prelude/tests/TaskSpec.hs
@@ -3,10 +3,13 @@ module TaskSpec (tests) where
 import qualified Expect
 import qualified List
 import NriPrelude
-import Prelude ((*>))
+import qualified Platform.Internal as Internal
+import Prelude ((*>), pure)
 import qualified Process
 import qualified Result
+import qualified System.TimeIt
 import Test (Test, describe, test)
+import Test.Internal (Failure)
 import qualified Task
 import qualified Tuple
 
@@ -15,24 +18,50 @@ tests =
   describe
     "Task"
     [ describe "parallel"
-        [ test "returns the right values" <| \() -> do
-            let results = [1, 2, 3]
-            let tasks = List.map waitForIt results
-            Task.parallel tasks
-              |> Expect.andCheck (Expect.equal results)
-        ]
+      [ test "returns the right values and executes in parallel" <| \() -> do
+        let results = [1, 2, 3]
+        let parallelTask = Task.parallel <| List.map afterDelay results
+
+        (singleExecutionTime, ()) <- Expect.succeeds <| timeIt <| afterDelay ()
+        (parallelExecutionTime, parallelResults) <- Expect.succeeds <| timeIt parallelTask
+
+        Expect.equal results parallelResults
+
+        parallelExecutionTime |>
+          Expect.all
+            [ Expect.atLeast singleExecutionTime
+            , Expect.lessThan (2 * singleExecutionTime)
+            ]
+      ]
     , describe "concurrently"
-        [ test "returns the right values" <| \() -> do
-            let results = (1, 2)
-            let (taskA, taskB) = Tuple.mapBoth waitForIt waitForIt results
-            Task.concurrently taskA taskB
-              |> Expect.andCheck (Expect.equal results)
-        ]
+      [ test "returns the right values and executes concurrently" <| \() -> do
+        let results = (1, "two")
+        let (taskA, taskB) = Tuple.mapBoth afterDelay afterDelay results
+        let concurrentTask = Task.concurrently taskA taskB
+
+        (singleExecutionTime, ()) <- Expect.succeeds <| timeIt <| afterDelay ()
+        (concurrentExecutionTime, concurrentResults) <- Expect.succeeds <| timeIt concurrentTask
+
+        Expect.equal results concurrentResults
+
+        concurrentExecutionTime |>
+          Expect.all
+            [ Expect.atLeast singleExecutionTime
+            , Expect.lessThan (2 * singleExecutionTime)
+            ]
+      ]
     ]
 
-waitForIt :: a -> Task x a
-waitForIt a =
-    Process.sleep waitMilliseconds *> Task.succeed a
+afterDelay :: a -> Task Failure a
+afterDelay a =
+  Process.sleep waitMilliseconds *> Task.succeed a
 
 waitMilliseconds :: Float
 waitMilliseconds = 100
+
+timeIt :: Task e a -> Task e (Float, a)
+timeIt task =
+  Internal.Task <| \handler -> do
+    (seconds, res) <- System.TimeIt.timeItT <| Internal._run task handler
+    let milliseconds = seconds * 1000
+    pure <| Result.map ((,) milliseconds) res
