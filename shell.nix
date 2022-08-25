29cff6145a3e658127581320079e031ecaadb54a
Caique Figueiredo <caiquefig95@gmail.com>
2022-08-25 15:47:01 -0300

    Default travis to ghc 9.2.4

--- shell.nix
+++ shell.nix
@@ -1,1 +1,16 @@
-shell-ghc-8-10.nix
+let
+  sources = import ./nix/sources.nix { };
+
+  pkgs = import sources.nixpkgs { };
+  pkgs-unstable = import sources.nixpkgs-unstable { };
+in import nix/mk-shell.nix {
+  pkgs = pkgs;
+  pkgs-unstable = pkgs-unstable;
+  haskellPackages = pkgs-unstable.haskell.packages.ghc924.extend (self: super: {
+    safe-coloured-text = super.callCabal2nix "safe-coloured-text"
+      "${sources.safe-coloured-text}/safe-coloured-text" { };
+    safe-coloured-text-terminfo =
+      super.callCabal2nix "safe-coloured-text-terminfo"
+      "${sources.safe-coloured-text}/safe-coloured-text-terminfo" { };
+  });
+}
