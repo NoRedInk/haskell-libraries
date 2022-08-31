23f3ea4331a42e1b5c8d9352746872465f479c7c
Juliano Solanho <juliano.solanho@gmail.com>
2022-08-31 10:20:24 -0300

    Ensure a frozen callstack for generated tests
    Before this commit, we effectively had this:
    
    a :: Stack.HasCallStack => IO ()
    a = do
      Stack.withFrozenCallStack b
    
    b :: IO ()
    b = do
      c
    
    c :: Stack.HasCallStack => IO ()
    c = do
      Stack.callStack & Stack.getCallStack & print
    
    And the effect of this, is in `c`, we get a callstack with a single
    frame: `b`'s call to `c`.
    
    We need to have `HasCallStack` all the way through for
    `Stack.withFrozenCallStack` to work.

--- nri-test-encoding/src/Test/Encoding/Redis.hs
+++ nri-test-encoding/src/Test/Encoding/Redis.hs
@@ -4,6 +4,7 @@ module Test.Encoding.Redis (test) where
 import Data.Proxy (Proxy (Proxy))
 import qualified Data.Typeable as Typeable
 import qualified Examples
+import qualified GHC.Stack as Stack
 import NriPrelude
 import Test (Test)
 import qualified Test.Encoding
@@ -16,8 +17,17 @@ import qualified Text
 -- 2. encoded the examples into JSON
 -- 3. check the encoded JSON against the generated file
 --   NOTE: it will generate the file if it doesn't exist yet
-test :: forall m key a. (Typeable.Typeable a, Examples.HasExamples a) => m key a -> Test
-test _ =
+test :: forall m key a. (Typeable.Typeable a, Examples.HasExamples a, Stack.HasCallStack) => m key a -> Test
+test proxy =
+  -- We need to freeze the call stack before proceeding.
+  -- If we don't, the callstack for these generated tests would point to haskell-libraries code
+  -- and not client code.
+  -- This didn't matter that much until we had the ability to specify `--files tests/BlaSpec.hs`
+  -- If we don't fix this, we can't ever `--files tests/SomeApiSpec.hs`
+  Stack.withFrozenCallStack frozenTest proxy
+
+frozenTest :: forall m key a. (Typeable.Typeable a, Examples.HasExamples a, Stack.HasCallStack) => m key a -> Test
+frozenTest _ =
   let proxy = Proxy :: Proxy a
       tyCon =
         Typeable.typeRep proxy
