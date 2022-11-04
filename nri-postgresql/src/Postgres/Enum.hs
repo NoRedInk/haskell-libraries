ef2e988bd350feea39d26478fcc2e09b373a1bb9
Micah Hahn <micah.s.hahn@gmail.com>
2022-11-04 14:34:28 -0500

    Refactoring + data type checks

--- nri-postgresql/src/Postgres/Enum.hs
+++ nri-postgresql/src/Postgres/Enum.hs
@@ -22,76 +22,103 @@ makeValueList :: List Text -> Text
 makeValueList list = 
   "[" ++ Text.join ", " (List.map quote list) ++ "]"
 
-generatePGEnum :: TH.Name -> Text -> [(a, Text)] -> TH.Q [TH.Dec]
-generatePGEnum name databaseTypeName mapping =
-  TH.runIO <| withTPGConnection (\connection -> do
-    let hsTypeName = "_"
+generatePGEnum :: (Ord a, Eq a) => TH.Name -> Text -> [(a, Text)] -> TH.Q [TH.Dec]
+generatePGEnum hsTypeName databaseTypeName mapping = do 
+  let hsTypeString = Prelude.show hsTypeName
 
-    let duplicateValues =
-          List.map Tuple.second mapping
-            |> List.sort 
-            |> group
-            |> List.filterMap 
-                (\items -> case items of 
-                            (item : _ : _) -> 
-                              Just item
+  info <- TH.reify hsTypeName
 
-                            _ ->
-                              Nothing
-                )
+  conNames <-
+    case info of 
+      TH.TyConI (TH.DataD _ _ _ _ constructors _) ->
+        constructors
+          |> Prelude.traverse (\con -> case con of 
+                TH.NormalC name [] ->
+                  Prelude.pure name
 
-    _ <- case duplicateValues of 
-      [] -> Prelude.pure () 
-      _ -> Prelude.fail <| "The following values in the mapping are duplicated: " ++ Text.toList (makeValueList duplicateValues)
+                TH.NormalC name _ ->
+                  Prelude.fail <| "Constructor '" ++ Prelude.show name ++ "' cannot have any arguments in order to be mapped to an enum."
 
-    -- Check if the databaseTypeName exists on the PG database and is an enum
-    -- See https://www.postgresql.org/docs/current/catalog-pg-type.html
-    (_, typeRows) <-
-      pgSimpleQuery connection (BSL.fromChunks
-        [ "SELECT typtype"
-        , " FROM pg_catalog.pg_type"
-        , " WHERE typname = '", Encoding.encodeUtf8 databaseTypeName, "'"
-        ])
+                _ -> 
+                  Prelude.fail <| "Data type '" ++ hsTypeString ++ "' must contain only simple constructors with no arguments."
+              )
 
-    _ <- case List.map (\[v] -> pgDecodeRep v) typeRows of 
-      [] -> 
-        Prelude.fail ("Type '" ++ Text.toList databaseTypeName ++ "' does not exist on the database.")
-      
-      -- 'e' means enum type
-      ['e'] -> 
-        Prelude.pure ()
-      
-      _ -> 
-        Prelude.fail ("Type '" ++ Text.toList databaseTypeName ++ "' is not an enum type.")
+      _ ->
+        Prelude.fail <| "The datatype name '" ++ hsTypeString ++ "' must be a data declaration."
 
-    (_, enumRows) <- 
-      pgSimpleQuery connection (BSL.fromChunks
-        [ "SELECT enumlabel"
-        , " FROM pg_catalog.pg_enum"
-        , " WHERE enumtypid = '", Encoding.encodeUtf8 databaseTypeName, "'::regtype"
-        , " ORDER BY enumsortorder"
-        ])
+  -- Check for duplicate values specified in the mapping
+  _ <- 
+    List.map Tuple.second mapping
+      |> List.sort 
+      |> group
+      |> List.filterMap 
+          (\items -> 
+            case items of 
+              (item : _ : _) -> 
+                Just item
+
+              _ ->
+                Nothing
+          )
+      |> (\duplicateValues -> 
+            case duplicateValues of 
+              [] -> 
+                Prelude.pure () 
+
+              _ -> 
+                Prelude.fail <| "The following values in the mapping are duplicated: " ++ Text.toList (makeValueList duplicateValues)  
+          )
+
+  (pgEnumValues :: List Text) <-
+    TH.runIO <| withTPGConnection (\connection -> do 
+      -- Check if the databaseTypeName exists on the PG database and is an enum
+      -- See https://www.postgresql.org/docs/current/catalog-pg-type.html
+      (_, typeRows) <-
+        pgSimpleQuery connection (BSL.fromChunks
+          [ "SELECT typtype"
+          , " FROM pg_catalog.pg_type"
+          , " WHERE typname = '", Encoding.encodeUtf8 databaseTypeName, "'"
+          ])
+
+      _ <- case List.map (\[v] -> pgDecodeRep v) typeRows of 
+        [] -> 
+          Prelude.fail ("Type '" ++ Text.toList databaseTypeName ++ "' does not exist on the database.")
         
-    (values :: List Text) <- 
+        -- 'e' means enum type
+        ['e'] -> 
+          Prelude.pure ()
+        
+        _ -> 
+          Prelude.fail ("Type '" ++ Text.toList databaseTypeName ++ "' is not an enum type.")
+
+      (_, enumRows) <- 
+        pgSimpleQuery connection (BSL.fromChunks
+          [ "SELECT enumlabel"
+          , " FROM pg_catalog.pg_enum"
+          , " WHERE enumtypid = '", Encoding.encodeUtf8 databaseTypeName, "'::regtype"
+          , " ORDER BY enumsortorder"
+          ])
+        
       case List.map (\[value] -> pgDecodeRep value) enumRows of 
         [] -> 
           Prelude.fail ("Enum type '" ++ Text.toList databaseTypeName ++ "' does not contain any values.")
+
         vs -> 
           Prelude.pure vs
+    )
 
-    let dbValuesSet = Set.fromList values
-    let mappingValuesSet = Set.fromList (List.map Tuple.second mapping)    
+  let pgValuesSet = Set.fromList pgEnumValues
+  let mappingValuesSet = Set.fromList (List.map Tuple.second mapping) 
 
-    _ <- 
-      case (Set.toList (Set.diff dbValuesSet mappingValuesSet), Set.toList (Set.diff mappingValuesSet dbValuesSet)) of 
-        ([], []) -> 
-          Prelude.pure ()
+  _ <- 
+    case (Set.toList (Set.diff pgValuesSet mappingValuesSet), Set.toList (Set.diff mappingValuesSet pgValuesSet)) of 
+      ([], []) -> 
+        Prelude.pure ()
 
-        ([], hsOnlyValues) ->
-          Prelude.fail <| "The following values of type '" ++ hsTypeName ++ "' are not mapped to values of pg enum type '" ++ Text.toList databaseTypeName ++ "': " ++ Text.toList (makeValueList hsOnlyValues)
+      ([], hsOnlyValues) ->
+        Prelude.fail <| "The following values of type '" ++ hsTypeString ++ "' are not mapped to values of pg enum type '" ++ Text.toList databaseTypeName ++ "': " ++ Text.toList (makeValueList hsOnlyValues)
 
-        (pgOnlyValues, _) -> 
-          Prelude.fail <| "The following values from the pg enum type '" ++ Text.toList databaseTypeName ++ "' are not mapped to values of " ++ hsTypeName ++ ": " ++ Text.toList (makeValueList pgOnlyValues)
+      (pgOnlyValues, _) -> 
+        Prelude.fail <| "The following values from the pg enum type '" ++ Text.toList databaseTypeName ++ "' are not mapped to values of " ++ hsTypeString ++ ": " ++ Text.toList (makeValueList pgOnlyValues)
 
-    Prelude.fail "TODO"
-  )
+  Prelude.fail "TODO"
