698b842d79d39d513adedd67064e348bebebe230
Micah Hahn <micah.s.hahn@gmail.com>
2023-02-24 15:35:36 -0600

    Fix test failing for the wrong reason

--- nri-postgresql/test/ConflictTest.hs
+++ nri-postgresql/test/ConflictTest.hs
@@ -7,11 +7,17 @@ import qualified Test
 import qualified Postgres
 import qualified Expect
 
-expectSuccess :: Result Postgres.Error [row] -> Task Postgres.Error [row]
+expectSuccess :: Result Postgres.Error [row] -> Task Text [row]
 expectSuccess result = 
   case result of 
     Ok rows -> Task.succeed rows
-    Err err -> Task.fail err
+    Err err -> Task.fail <| "Unexpected postgres error: " ++ (Debug.toString err)
+
+expectError :: Result Postgres.Error [row] -> Task Text Postgres.Error
+expectError result =
+  case result of 
+    Ok _ -> Task.fail "Expected to encounter an error"
+    Err err -> Task.succeed err
 
 tests :: Postgres.Connection -> Test
 tests rawConnection =
@@ -22,7 +28,7 @@ tests rawConnection =
             rawConnection
             (\connection -> do
               _ <- Postgres.doQuery connection [Postgres.sql| INSERT INTO constraints_table (user_id) VALUES (1) |] expectSuccess
-              _ <- Postgres.doQuery connection [Postgres.sql| INSERT INTO constraints_table (user_id) VALUES (1) |] expectSuccess
+              _ <- Postgres.doQuery connection [Postgres.sql| INSERT INTO constraints_table (user_id) VALUES (1) |] expectError
               Task.succeed ()
             )
             |> Expect.succeeds
