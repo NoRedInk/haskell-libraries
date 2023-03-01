d784107f887e6e429a6b8cb440501b4300bd0ed1
Stoeffel <schtoeffel@gmail.com>
2023-03-01 10:33:29 +0100

    Simplify conflict test

--- nri-postgresql/test/ConflictTest.hs
+++ nri-postgresql/test/ConflictTest.hs
@@ -4,32 +4,18 @@ module ConflictTest where
 
 import qualified Expect
 import qualified Postgres
+import qualified Postgres.Test
 import Test (Test)
 import qualified Test
 
-expectSuccess :: Result Postgres.Error [row] -> Task Text [row]
-expectSuccess result =
-  case result of
-    Ok rows -> Task.succeed rows
-    Err err -> Task.fail <| "Unexpected postgres error: " ++ (Debug.toString err)
-
-expectError :: Result Postgres.Error [row] -> Task Text Postgres.Error
-expectError result =
-  case result of
-    Ok _ -> Task.fail "Expected to encounter an error"
-    Err err -> Task.succeed err
-
-tests :: Postgres.Connection -> Test
-tests rawConnection =
+tests :: a -> Test
+tests _ =
   Test.describe
     "Postgres.Test"
-    [ Test.test "test transaction can recover from a conflict" <| \_ ->
-        Postgres.inTestTransaction
-          rawConnection
-          ( \connection -> do
-              _ <- Postgres.doQuery connection [Postgres.sql| INSERT INTO constraints_table (user_id) VALUES (1) |] expectSuccess
-              _ <- Postgres.doQuery connection [Postgres.sql| INSERT INTO constraints_table (user_id) VALUES (1) |] expectError
-              Task.succeed ()
-          )
-          |> Expect.succeeds
+    [ Postgres.Test.test "test transaction can recover from a conflict" <| \connection -> do
+        _ <-
+          Postgres.doQuery connection [Postgres.sql| INSERT INTO constraints_table (user_id) VALUES (1) |] Task.succeed
+            |> Expect.andCheck Expect.ok
+        Postgres.doQuery connection [Postgres.sql| INSERT INTO constraints_table (user_id) VALUES (1) |] Task.succeed
+          |> Expect.andCheck Expect.err
     ]
