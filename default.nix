b0f6125a6ed71bd552845e60f27533229911049a
ilias <mail@ilias.xyz>
2023-04-09 01:56:01 +0200

    Make log-explorer work

--- default.nix
+++ default.nix
@@ -1,7 +1,8 @@
 let
   sources = import ./nix/sources.nix { };
   pkgs = import sources.nixpkgs { };
-  haskellPackages = pkgs.haskellPackages.extend (self: super: {
+  pkgs-unstable = import sources.nixpkgs-unstable { };
+  haskellPackages = pkgs-unstable.haskell.packages.ghc924.extend (self: super: {
     nri-prelude = super.callCabal2nix "nri-prelude" ./nri-prelude { };
     log-explorer = super.callCabal2nix "log-explorer" ./nri-log-explorer { };
     safe-coloured-text = super.callCabal2nix "safe-coloured-text"
@@ -10,8 +11,7 @@ let
       super.callCabal2nix "safe-coloured-text-terminfo"
       "${sources.safe-coloured-text}/safe-coloured-text-terminfo" { };
   });
-
 in {
   log-explorer =
-    pkgs.haskell.lib.justStaticExecutables haskellPackages.log-explorer;
+    pkgs-unstable.haskell.lib.justStaticExecutables haskellPackages.log-explorer;
 }
