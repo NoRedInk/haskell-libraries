b56cfc88115cca08a2b9fe71c7ef969d573ff78a
Caique Figueiredo <caiquefig95@gmail.com>
2023-04-26 17:00:16 -0300

    pushy push

--- default.nix
+++ default.nix
@@ -2,12 +2,17 @@ let
   sources = import ./nix/sources.nix { };
   pkgs = import sources.nixpkgs { };
   haskellPackages = pkgs.haskellPackages.extend (self: super: {
-    nri-prelude = super.callCabal2nix "nri-prelude" ./nri-prelude { };
-    log-explorer = super.callCabal2nix "log-explorer" ./nri-log-explorer { };
-    safe-coloured-text = super.callCabal2nix "safe-coloured-text"
+    mkDerivation = expr:
+      super.mkDerivation (expr // {
+        enableLibraryProfiling = true;
+        enableExecutableProfiling = true;
+      });
+    nri-prelude = self.callCabal2nix "nri-prelude" ./nri-prelude { };
+    log-explorer = self.callCabal2nix "log-explorer" ./nri-log-explorer { };
+    safe-coloured-text = self.callCabal2nix "safe-coloured-text"
       "${sources.safe-coloured-text}/safe-coloured-text" { };
     safe-coloured-text-terminfo =
-      super.callCabal2nix "safe-coloured-text-terminfo"
+      self.callCabal2nix "safe-coloured-text-terminfo"
       "${sources.safe-coloured-text}/safe-coloured-text-terminfo" { };
   });
 
