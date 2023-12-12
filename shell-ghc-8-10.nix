c2182c1476bd770a987590fe390c4beb6a9ae0a8
Jinnah Ali-Clarke <jinnah@noredink.com>
2023-12-12 15:10:56 -0500

    pin brick to 0.71.1; many breaking changes from 0.72 onward

--- shell-ghc-8-10.nix
+++ shell-ghc-8-10.nix
@@ -8,5 +8,8 @@ in import nix/mk-shell.nix {
     # modern-uri-0.3.6.0 is incompatible with the `base` provided by ghc 8.10.7;
     # use known-good @ 0.3.4.4.
     modern-uri = self.callHackage "modern-uri" "0.3.4.4" { };
+
+    # todo: resolve breaking changes in brick >= 0.72
+    brick = self.callHackage "brick" "0.71.1" { };
   });
 }
