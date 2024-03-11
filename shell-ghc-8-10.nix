82ac691eec8c0f601e53320740ab6b821c95e9eb
Juliano Solanho <juliano.solanho@gmail.com>
2024-03-11 12:39:13 -0700

    failed attempt at patching rdkafka

--- shell-ghc-8-10.nix
+++ shell-ghc-8-10.nix
@@ -1,7 +1,31 @@
 let
   sources = import ./nix/sources.nix { };
 
-  pkgs = import sources.nixpkgs { };
+  pkgs = import sources.nixpkgs {
+    ovelays = [
+      (self: super: {
+        rdkafka = self.stdenv.mkDerivation rec {
+          pname = "rdkafka";
+          version = "2.2.0";
+
+          # git clone https://github.com/confluentinc/librdkafka ../librdkafka
+          src = ../librdkafka;
+
+          nativeBuildInputs = with self.pkgs; [ pkg-config python3 which ];
+
+          buildInputs = with self.pkgs; [ zlib zstd openssl ];
+
+          env.NIX_CFLAGS_COMPILE = "-Wno-error=strict-overflow";
+
+          postPatch = ''
+            patchShebangs .
+          '';
+
+          enableParallelBuilding = true;
+        };
+      })
+    ];
+  };
 in import nix/mk-shell.nix {
   pkgs = pkgs;
   haskellPackages = pkgs.haskell.packages.ghc8107.extend (self: super: {
