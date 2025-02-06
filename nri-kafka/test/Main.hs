592a9307ec89bcb0fd64dfe648a52837f5fc4009
Juliano Solanho <juliano.solanho@gmail.com>
2025-02-06 19:43:34 -0300

    try raising the timeout for nri-kafka

--- nri-kafka/test/Main.hs
+++ nri-kafka/test/Main.hs
@@ -2,11 +2,15 @@ module Main (main) where
 
 import qualified Spec.Kafka.Worker.Integration
 import qualified Spec.Kafka.Worker.Partition
+import qualified System.Environment
 import qualified Test
 import qualified Prelude
 
 main :: Prelude.IO ()
-main = Test.run tests
+main = do
+  -- macos runners seem to be slow and fail on several kafka integration tests
+  System.Environment.setEnv "NRI_TEST_TIMEOUT" "20000"
+  Test.run tests
 
 tests :: Test.Test
 tests =
