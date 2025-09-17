50daea56d7d84e35343ad0d776eeac9495bbf13f
Juliano Solanho <juliano.solanho@gmail.com>
2025-09-17 18:11:05 -0300

    fix test: don't acquire conduit every time

--- nri-observability/scripts/memory-leak-test/Main.hs
+++ nri-observability/scripts/memory-leak-test/Main.hs
@@ -19,21 +19,20 @@ main :: IO ()
 main = do
   settings' <- Environment.decode Observability.decoder
   putStrLn (show settings'.enabledReporters)
-  -- [0 .. 1000]
-  [0..10000]
-    |> List.map Text.fromInt
-    |> mapConcurrently (\requestId -> do
-      Conduit.withAcquire (Observability.handler settings') <| \handler -> do
-        Platform.rootTracingSpanIO
-            requestId
-            (Observability.report handler requestId)
-            ("Running task" ++ requestId)
-            ( \log -> do
-                Task.perform log (do
-                  Process.sleep 5
-                  Task.succeed ())
-            )
-    )
-    |> void
+  Conduit.withAcquire (Observability.handler settings') <| \handler -> do
+    [0..300_000]
+      |> List.map Text.fromInt
+      |> mapConcurrently (\requestId -> do
+          Platform.rootTracingSpanIO
+              requestId
+              (Observability.report handler requestId)
+              ("Running task" ++ requestId)
+              ( \log -> do
+                  Task.perform log (do
+                    Process.sleep 5
+                    Task.succeed ())
+              )
+      )
+      |> void
   -- give async threads 1s to finish
-  threadDelay 1_000_000
+  -- threadDelay 5_000_000
