2648c6fb5554a3a46cb7c9be82b0e81e32173292
Juliano Solanho <juliano.solanho@gmail.com>
2025-09-30 14:41:47 -0300

    don't re-advertise log-explorer on the same pause

--- nri-observability/src/Reporter/Dev/Internal.hs
+++ nri-observability/src/Reporter/Dev/Internal.hs
@@ -87,7 +87,7 @@ handler = do
   writeLock <- MVar.newEmptyMVar
   counter <- MVar.newMVar 0
   loggingThread <- Async.async (logLoop counter writeLock)
-  advertiseLogExplorer <- Async.async (advertiseLoop counter)
+  advertiseLogExplorer <- Async.async (advertiseLoop counter Nothing)
   timer <- Timer.mkTimer
   Prelude.pure Handler {timer, writeLock, loggingThread, advertiseLogExplorer}
 
@@ -107,13 +107,17 @@ logLoop counter lock = do
   MVar.modifyMVar_ counter (\n -> Prelude.pure (n + 1))
   logLoop counter lock
 
-advertiseLoop :: MVar.MVar Int -> Prelude.IO ()
-advertiseLoop counter = do
+advertiseLoop :: MVar.MVar Int -> Maybe Int -> Prelude.IO ()
+advertiseLoop counter lastAdvertisedCount = do
   lastCount <- MVar.readMVar counter
   Control.Concurrent.threadDelay 3_000_000 {- 3 seconds -}
   currentCount <- MVar.readMVar counter
-  if lastCount == currentCount && currentCount > 0
+  -- if after 3 seconds the count hasn't changed, we've stopped logging for a bit
+  let stoppedLogging = lastCount == currentCount
+  -- we don't want to advertise multiple times while we're idle
+  let haventAdvertisedAtCurrentCount = lastAdvertisedCount /= Just currentCount
+  if stoppedLogging && haventAdvertisedAtCurrentCount
     then do
       putTextLn "🕵️ Need more detail? Try running the `log-explorer` command!\n"
-      advertiseLoop counter
-    else advertiseLoop counter
+      advertiseLoop counter (Just currentCount)
+    else advertiseLoop counter lastAdvertisedCount
